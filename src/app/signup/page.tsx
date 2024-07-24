7dfab5cd08464b999b1418a5b8e41ade0ed341fd
Modupe Akanni <modupe775@gmail.com>
2024-07-24 02:05:54 +0100

    feat: implement sign-up

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,100 @@
-import Image from "next/image"
-import Link from "next/link"
+'use client';
+
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
+import { supabase } from '@/client';
 
 const Page = () => {
+    const router = useRouter();
+    const [isFormValid, setIsFormValid] = useState(false);
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+        confirmPassword: '',
+    });
+
+    const [errors, setErrors] = useState({
+        email: '',
+        password: '',
+        confirmPassword: '',
+    });
+
+    const [showSuccessPopup, setShowSuccessPopup] = useState(false);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+        setErrors({ ...errors, [name]: '' });
+    };
+
+    const validate = () => {
+        let valid = true;
+        let errors = {
+            email: '',
+            password: '',
+            confirmPassword: '',
+        };
+
+        if (!formData.email) {
+            errors.email = 'Cannot be empty';
+            valid = false;
+        } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+            errors.email = "Invalid email format";
+            valid = false;
+        }
+
+        if (!formData.password) {
+            errors.password = 'Please check again';
+            valid = false;
+        } else if (formData.password.length < 8) {
+            errors.password = "Password too short";
+            valid = false;
+        }
+
+        if (formData.password !== formData.confirmPassword) {
+            errors.confirmPassword = 'Passwords do not match';
+            valid = false;
+        }
+
+        setErrors(errors);
+        return valid;
+    };
+
+    useEffect(() => {
+        const isValid = validate();
+        setIsFormValid(isValid);
+    }, [formData]);
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!validate()) return;
+
+        const { email, password } = formData;
+        if (supabase?.auth) {
+            const { data, error } = await supabase.auth.signUp({ email, password });
+
+            if (error) {
+                alert(error.message);
+            } else {
+                alert('User created successfully!');
+                if (data.session) {
+                    const { access_token } = data.session;
+                    sessionStorage.setItem('access_token', access_token);
+                    localStorage.setItem('access_token', access_token);
+                }
+                setShowSuccessPopup(true);
+                setTimeout(() => {
+                    router.push('/');
+                }, 2000);
+            }
+        } else {
+            alert('Supabase auth is not initialized.');
+        }
+    };
+
     return (
         <div className="min-h-screen mt-32 mb-12 flex items-center justify-center">
             <div className="flex min-w-[396px] flex-col items-center">
@@ -12,38 +105,123 @@ const Page = () => {
                 <div className="w-full">
                     <div className="text-left mt-10">
                         <h1 className="text-3xl font-bold mb-2">Create account</h1>
-                        <p className="text-[#737373] mb-10">Let's get you started sharing your links!</p>
+                        <p className="text-[#737373]">
+                            Let's get you started sharing your links!
+                        </p>
                     </div>
-                    <form className="space-y-6 flex flex-col w-full">
+                    <form
+                        onSubmit={handleSubmit}
+                        className="mt-10 space-y-6 flex flex-col w-full"
+                    >
                         <div>
-                            <label htmlFor="email" className="mb-1 text-xs text-[#333333]">Email address</label>
+                            <label
+                                htmlFor="email"
+                                className={`mb-1 text-xs ${errors.email ? 'text-red-500' : 'text-[#333333]'
+                                    }`}
+                            >
+                                Email address
+                            </label>
                             <div className="relative">
-                                <input name="email" id="email" type="email" placeholder="e.g. alex@email.com" className="w-full border rounded-md pl-11 p-2 outline-[#633CFF]" />
-                                <Image src="/mail.svg" alt="mail" width={16} height={16} className="w-4 h-4  absolute top-2.5 left-4" />
+                                <input
+                                    name="email"
+                                    id="email"
+                                    type="email"
+                                    placeholder="e.g. alex@email.com"
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                    className={`w-full border rounded-md pl-11 p-2 outline-[#633CFF] ${errors.email ? 'border-red-500' : ''}`}
+                                />
+                                <Image
+                                    src="/mail.svg"
+                                    alt="mail"
+                                    width={16}
+                                    height={16}
+                                    className="w-4 h-4  absolute top-2.5 left-4"
+                                />
+                                {errors.email && <span className="text-red-500 text-xs absolute right-2.5 top-2.5">{errors.email}</span>}
                             </div>
                         </div>
                         <div>
-                            <label htmlFor="password" className="mb-1 text-xs text-[#333333]">Create password</label>
+                            <label htmlFor="password" className={`mb-1 text-xs ${errors.password ? 'text-red-500' : 'text-[#333333]'}`}>
+                                Create password
+                            </label>
                             <div className="relative">
-                                <input type="password" name="password" id="password" placeholder="At least 8 characters" className="w-full border rounded-md pl-11 p-2 outline-[#633CFF]" />
-                                <Image src="/password.svg" alt="lock" width={16} height={16} className="w-4 h-4  absolute top-2.5 left-4" />
+                                <input
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    placeholder="At least 8 characters"
+                                    value={formData.password}
+                                    onChange={handleChange}
+                                    className={`w-full border rounded-md pl-11 p-2 outline-[#633CFF] ${errors.password ? 'border-red-500' : ''}`}
+                                />
+                                <Image
+                                    src="/password.svg"
+                                    alt="lock"
+                                    width={16}
+                                    height={16}
+                                    className="w-4 h-4  absolute top-2.5 left-4"
+                                />
+                                {errors.password && <span className="text-red-500 text-xs absolute right-2.5 top-2.5">{errors.password}</span>}
                             </div>
                         </div>
                         <div>
-                            <label htmlFor="confirmPassword" className="mb-1 text-xs text-[#333333]">Confirm password</label>
+                            <label
+                                htmlFor="confirmPassword"
+                                className={`mb-1 text-xs ${errors.confirmPassword ? 'text-red-500' : 'text-[#333333]'}`}
+                            >
+                                Confirm password
+                            </label>
                             <div className="relative">
-                                <input type="password" name="confirmPassword" id="confirmPassword" placeholder="At least 8 characters" className="w-full border rounded-md pl-11  p-2 outline-[#633CFF]" />
-                                <Image src="/password.svg" alt="lock" width={16} height={16} className="w-4 h-4 absolute top-2.5 left-4" />
+                                <input
+                                    type="password"
+                                    name="confirmPassword"
+                                    id="confirmPassword"
+                                    placeholder="At least 8 characters"
+                                    value={formData.confirmPassword}
+                                    onChange={handleChange}
+                                    className={`w-full border rounded-md pl-11 p-2 outline-[#633CFF] ${errors.confirmPassword ? 'border-red-500' : ''}`}
+                                />
+                                <Image
+                                    src="/password.svg"
+                                    alt="lock"
+                                    width={16}
+                                    height={16}
+                                    className="w-4 h-4 absolute top-2.5 left-4"
+                                />
+                                {errors.confirmPassword && <span className="text-red-500 text-xs absolute right-2.5 top-2.5">{errors.confirmPassword}</span>}
                             </div>
                         </div>
-                        <p className="text-xs text-[#737373]">Password must contain at least 8 characters</p>
-                        <button className="w-full bg-[#633CFF] font-semibold text-base text-white rounded-md p-2">Create new account</button>
+                        <p className="text-xs text-[#737373]">
+                            Password must contain at least 8 characters
+                        </p>
+                        <button
+                            type="submit"
+                            disabled={!isFormValid}
+                            className={`w-full font-semibold text-base rounded-md p-2  text-white ${!isFormValid ? "bg-[#BEADFF] cursor-not-allowed" : "bg-[#633CFF]"
+                                }`}
+                        >
+                            Create new account
+                        </button>
                     </form>
                 </div>
-                <p className="text-sm pt-6">Already have an account? <span className="text-[#633CFF] cursor-pointer"><Link href="/login">Login</Link></span></p>
+                <p className="text-sm pt-6">
+                    Already have an account?{' '}
+                    <span className="text-[#633CFF] cursor-pointer">
+                        <Link href="/login">Login</Link>
+                    </span>
+                </p>
             </div>
+            {showSuccessPopup && (
+                <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+                    <div className="bg-white p-6 rounded-md shadow-md text-center">
+                        <p className="text-lg font-semibold">You have signed up successfully!</p>
+                        <p className="text-sm mt-2">You will be redirected to your dashboard shortly.</p>
+                    </div>
+                </div>
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default Page
+export default Page;
