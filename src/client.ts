06451b4c1170c191038cf2a946fc0a04863e510d
Modupe Akanni <modupe775@gmail.com>
2024-07-25 09:13:55 +0100

    fix: update client

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const API_KEY = process.env.NEXT_PUBLIC_SUPABASE_API_KEY;
-
+const URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const API_KEY = process.env.NEXT_PUBLIC_SUPABASE_API_KEY as string;
 
 export const supabase = createClient(URL, API_KEY);
